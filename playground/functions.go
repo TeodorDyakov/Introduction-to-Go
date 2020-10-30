cda0452e36b3ebfe41fbcc46c0dac81cbb319b3b
TeodorDyakov <tedy1996@gmail.com>
2020-10-30 03:36:28 +0200

    just fooling around

--- playground/functions.go
+++ playground/functions.go
@@ -31,24 +31,44 @@ func seq() func() int{
 	}
 }
 
+var n int = 5
+
+func global() func() int{
+	return func() int{
+		return n
+	}
+}
+
 func main() {
-	var f = op("+")
+	var f func(int, int) int = op("+")
 	fmt.Println(f(2,3))
 	//5
     fmt.Println(op("*")(2,3))
     //6
-    var p = seq()
+    
+    var p func() int = seq()
     fmt.Println(p())
     //1
     fmt.Println(p())
     //2
-    fmt.Println(p())
-    //3
+    
     p = seq()
     fmt.Println(p())
     //1
     fmt.Println(p())
     //2
-    fmt.Println(p())
-    //3
+  	x := p
+  	fmt.Println(x())
+    //3  
+
+    g := global()
+    fmt.Println(g())
+    //5
+    n = 7
+    fmt.Println(g())
+    //7
 }
+
+
+
+
