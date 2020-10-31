0b3c3bbb8dd7a930593cd1debd740b78c3d56c81
TeodorDyakov <tedy1996@gmail.com>
2020-11-01 00:27:26 +0200

    defer

--- playground/defer.go
+++ playground/defer.go
@@ -1,6 +1,6 @@
 package main
 import "fmt"
-
+import "math"
 var n int = 4;
 var b = 1;
 func square(){
@@ -17,4 +17,9 @@ func f(){
  func main(){
  	f();
  	fmt.Println("value of n after f: ", n)
+ 	fmt.Println(real(3i+4))
+ 	//функциите се изпълняват в обратен ред на ивикване
+ 	for i := 0; i < 10; i++{
+ 		defer fmt.Println(i)
+ 	}
  }
