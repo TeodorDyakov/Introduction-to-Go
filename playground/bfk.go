91c31c5e045f0c08056d1e5449ed1336b51f447d
TeodorDyakov <tedy1996@gmail.com>
2020-11-04 03:27:36 +0200

    bf

--- playground/bfk.go
+++ playground/bfk.go
@@ -8,12 +8,12 @@ import (
 
 func main(){
 	var arr[30000]byte 
-	var ptr, insPtr int
-	reader := bufio.NewReader(os.Stdin)
+	ptr, insPtr := 0, 0
+	in := bufio.NewReader(os.Stdin)
 	prog := []byte(os.Args[1])
 
-	jump := func(step int){
-		for brackets := 1; brackets != 0; {
+	jump := func(step int, do bool){
+		for brackets := 1; do && brackets != 0; {
 			insPtr += step
 			switch (prog[insPtr]) {
 				case '[' : brackets += step
@@ -21,22 +21,17 @@ func main(){
 			}	
 		}			
 	}
+
 	for ; insPtr < len(prog); insPtr++ {
 		switch prog[insPtr] {
 			case '>' : ptr++
 			case '<' : ptr--
 			case '+' : arr[ptr]++
 			case '-' : arr[ptr]--
-			case '[' :
-				if arr[ptr] == 0{
-					jump(1)
-				}
-			case ']' :
-				if arr[ptr] != 0{
-					jump(-1)
-				}
+			case '[' : jump(1, arr[ptr] == 0)
+			case ']' : jump(-1, arr[ptr] != 0)
 			case ',' : 
-				input, _ := reader.ReadString('\n')
+				input, _ := in.ReadString('\n')
 				arr[ptr] = ([]byte(input))[0]
 			case '.' : fmt.Printf(string(arr[ptr]))
 		}
